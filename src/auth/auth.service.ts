bd22b6aa0f91b7c59fec7cc57e0e493ffbacc0e2
tanbin.hossain <tanbin.hossain@trucklagbe.com>
2023-01-25 17:07:14 +0600

    init commit

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,24 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
-  login() {
+  constructor(private prismaService: PrismaService) {}
 
+  async singup(dto: AuthDto) {
+    const { email, password } = dto;
+    const hashPassword = await argon.hash(password);
+    return this.prismaService.user.create({
+      data: {
+        email: email,
+        password: hashPassword,
+      },
+    });
   }
 
-  singup() {
-    
+  signin() {
+    return 'sign in';
   }
 }
